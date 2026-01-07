631fea8ea992a09d9dd40103354abee622ee31c4
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2026-01-06 23:55:14 -0600

    Added EXP calculator

---
+++ EXP Calculator/main.py
@@ -0,0 +1,28 @@
+def main():
+    STARTING_LEVEL, ENDING_LEVEL = get_levels()
+    EXP_SUM = 0
+    for i in range(STARTING_LEVEL, ENDING_LEVEL):
+         EXP_SUM += i*1000
+    print(f"The total amount of EXP to go from {STARTING_LEVEL} to {ENDING_LEVEL} is {EXP_SUM}.")
+
+def get_levels():
+    while True:
+            try:
+                STARTING_LEVEL = int(input("What level are we starting from? "))
+                break
+            except ValueError:
+                print("Please only put a number in.")
+    while True:
+            try:
+                ENDING_LEVEL = int(input("Enter the ending level. "))
+                break
+            except ValueError:
+                print("Please only put a number in.")
+    if STARTING_LEVEL > ENDING_LEVEL:
+         print("Please make sure the starting level is less than the ending number.")
+         STARTING_LEVEL, ENDING_LEVEL = get_levels()
+
+    return STARTING_LEVEL, ENDING_LEVEL
+
+if __name__ == "__main__":
+    main()
